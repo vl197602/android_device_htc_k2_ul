956f28a4a5010f5f285cdc6171b163afbdedbe2d
qwerty <qwerty@qwerty-System-Product-Name.(none)>
2013-10-12 00:12:29 +0400

    rebuild

---
+++ cm.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/htc/k2_ul/full_k2_ul.mk)
+
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+PRODUCT_RELEASE_NAME := k2_ul
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/k2_ul/device.mk)
+
+# Device naming
+PRODUCT_DEVICE := k2_ul
+PRODUCT_NAME := cm_k2_ul
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One SV
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=k2_ul BUILD_ID=JDQ39 BUILD_FINGERPRINT="htc/k2_ul/htc_k2_ul:4.2.2/JDQ39/224405.3:user/release-keys" PRIVATE_BUILD_DESC="1.24.502.3 CL224405 release-keys"
---
+++ device.mk
@@ -0,0 +1,121 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# common msm8960 configs
+$(call inherit-product, device/htc/msm8960-common/msm8960.mk)
+
+# overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.k2_ul \
+    # init.qcom.mdm_links.sh \
+    init.k2_ul.rc \
+    init.k2_ul.usb.rc \
+    remount.k2_ul \
+    ueventd.k2_ul.rc
+
+# Post boot service
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.post_boot.sh:system/etc/init.post_boot.sh
+
+# QC thermald config
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
+
+# Vold config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
+
+# PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_PACKAGES += \
+    libnetcmdiface
+
+# Wifi config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/calibration:/system/etc/calibration
+
+PRODUCT_COPY_FILES += \
+    device/htc/k2_ul/modules/bcmdhd.ko:/system/lib/modules//bcmdhd.ko \
+    device/htc/k2_ul/modules/ansi_cprng.ko:/system/lib/modules/ansi_cprng.ko \
+    device/htc/k2_ul/modules/bluetooth-power.ko:/system/lib/modules/bluetooth-power.ko \
+    device/htc/k2_ul/modules/gspca_main.ko:/system/lib/modules/gspca_main.ko \
+   device/htc/k2_ul/modules/lcd.ko:/system/lib/modules/lcd.ko \
+   device/htc/k2_ul/modules/msm-buspm-dev.ko:/system/lib/modules/msm-buspm-dev.ko \
+  device/htc/k2_ul/modules/reset_modem.ko:/system/lib/modules/reset_modem.ko
+
+# Audio config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/AudioBTID.csv:system/etc/AudioBTID.csv \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/dsp/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar
+
+# Media config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# Keylayouts and Keychars
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/device-keypad.kl:system/usr/keylayout/device-keypad.kl \
+    $(LOCAL_PATH)/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
+    $(LOCAL_PATH)/keylayout/sr_touchscreen.kl:system/usr/keylayout/sr_touchscreen.kl \
+    $(LOCAL_PATH)/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl
+
+# Input device config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/projector_input.idc:system/usr/idc/projector_input.idc \
+    $(LOCAL_PATH)/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8960
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8960
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb
+
+# Set build date
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi 
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/htc/vendor_onesv/k2_ul-vendor.mk)
