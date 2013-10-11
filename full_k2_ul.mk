30c7c9583cba5e8bbb37f21ec6a71f70993e46f7
vl197602 <radarga@gmail.com>
2013-10-12 00:19:16 +0400

    Update full_k2_ul.mk

--- full_k2_ul.mk
+++ full_k2_ul.mk
@@ -25,7 +25,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from m4 device
+# Inherit from k2_ul device
 $(call inherit-product, device/htc/k2_ul/device.mk)
 
 # Set those variables here to overwrite the inherited values.
